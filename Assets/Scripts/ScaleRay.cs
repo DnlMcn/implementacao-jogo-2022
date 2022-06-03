dbd3076114aa8b78b790952de1f6b2166e0a72a5
Danilo Mancini <haomakk.himself@gmail.com>
2022-06-03 11:31:24 -0300

    Add scale change rate buttons

--- Assets/Scripts/ScaleRay.cs
+++ Assets/Scripts/ScaleRay.cs
@@ -7,8 +7,15 @@ public class ScaleRay : MonoBehaviour
 {
     Transform lastHit;
     Rigidbody lastHitRb;
+
     const float SCALING_RATE = 1.5f;
+    const float SCALING_RATE_LOW = 0.25f;
+    const float SCALING_RATE_HIGH = 5f;
+
     Vector3 scaleChange;
+    bool isLowChange = false;
+    bool isHighChange = false;
+
     public float maxScale = 40f;
     public float minScale = 0.05f;
 
@@ -17,6 +24,33 @@ public class ScaleRay : MonoBehaviour
     bool hasLeftClicked;
     bool hasRightClicked;
 
+    void TryLowChange()
+    {
+        if (Input.GetKeyDown("q")) 
+        { 
+            isLowChange = true; 
+            Debug.Log("Low change active."); 
+        }
+        else if (Input.GetKeyUp("q"))
+        {
+            isLowChange = false;
+            Debug.Log("Low change inactive.");
+        }
+    }
+
+    void TryHighChange()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            isHighChange = true;
+            Debug.Log("High change active.");
+        }
+        else if (Input.GetKeyUp("q"))
+        {
+            isHighChange = false; 
+            Debug.Log("High change inactive.");
+        }
+    }
 
     void TryShoot()
     {
@@ -30,9 +64,22 @@ public class ScaleRay : MonoBehaviour
                 {
                     lastHit = hit.collider.transform;
                     lastHitRb = hit.collider.gameObject.GetComponent<Rigidbody>();
-                    if (lastHit.localScale.y <= maxScale) 
+                    if (lastHit.localScale.y <= maxScale && !isLowChange && !isHighChange) 
                     {
-                        scaleChange = lastHit.localScale * SCALING_RATE;
+                        scaleChange = lastHit.localScale * SCALING_RATE * Time.deltaTime;
+                        Debug.Log("Scaling up normally.");
+                        ScaleChange(0);
+                    }
+                    else if (lastHit.localScale.y <= maxScale && isLowChange) 
+                    {
+                        scaleChange = lastHit.localScale * SCALING_RATE_LOW * Time.deltaTime;
+                        Debug.Log("Scaling up with low change.");
+                        ScaleChange(0);
+                    }
+                    else if (lastHit.localScale.y <= maxScale && isHighChange) 
+                    {
+                        scaleChange = lastHit.localScale * SCALING_RATE_HIGH * Time.deltaTime;
+                        Debug.Log("Scaling up with high change.");
                         ScaleChange(0);
                     }
                 }
@@ -49,9 +96,22 @@ public class ScaleRay : MonoBehaviour
                 {
                     lastHit = hit.collider.transform;
                     lastHitRb = hit.collider.gameObject.GetComponent<Rigidbody>();
-                    if (lastHit.localScale.y >= minScale) 
+                    if (lastHit.localScale.y >= minScale && !isLowChange && !isHighChange) 
                     {
-                        scaleChange = lastHit.localScale * SCALING_RATE;
+                        scaleChange = lastHit.localScale * SCALING_RATE * Time.deltaTime;
+                        Debug.Log("Scaling down normally.");
+                        ScaleChange(1);
+                    }
+                    else if (lastHit.localScale.y >= minScale && isLowChange)  
+                    {
+                        scaleChange = lastHit.localScale * SCALING_RATE_LOW * Time.deltaTime;
+                        Debug.Log("Scaling down with low change.");
+                        ScaleChange(1);
+                    }
+                    else if (lastHit.localScale.y >= minScale && isHighChange) 
+                    {
+                        scaleChange = lastHit.localScale * SCALING_RATE_HIGH * Time.deltaTime;
+                        Debug.Log("Scaling down with high hange.");
                         ScaleChange(1);
                     }
                 }
@@ -64,14 +124,14 @@ public class ScaleRay : MonoBehaviour
         switch (type)
         {
             case 1:
-                lastHit.localScale -= scaleChange * Time.deltaTime;
-                lastHitRb.mass -= scaleChange.y * Time.deltaTime;
+                lastHit.localScale -= scaleChange;
+                lastHitRb.mass -= scaleChange.y;
                 if (!hasRightClicked) OnRightMouseClick?.Invoke();
                 hasRightClicked = true;
                 break;
             case 0:
-                lastHit.localScale += scaleChange * Time.deltaTime;
-                lastHitRb.mass += scaleChange.y * Time.deltaTime;
+                lastHit.localScale += scaleChange;
+                lastHitRb.mass += scaleChange.y;
                 if (!hasLeftClicked) OnLeftMouseClick?.Invoke();
                 hasLeftClicked = true;
                 break;
@@ -82,6 +142,9 @@ public class ScaleRay : MonoBehaviour
 
     void Update()
     {
+        TryLowChange();
+        TryHighChange();
+        if (isLowChange && isHighChange) { isLowChange= false; isHighChange = false; }
         TryShoot();
     }
 }
